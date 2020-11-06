e7285eb0ae6575a650b0f75cb23206a0fb734534
huangxu <huangxu@itcast.cn>
2020-11-06 17:05:30 +0800

    feat: :fire:项目初始化

---
+++ src/router/index.ts
@@ -0,0 +1,10 @@
+import type { RouteRecordRaw } from 'vue-router'
+
+import { App } from 'vue'
+
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const router = createRouter({
+  history: createWebHashHistory(),
+  routes: []
+})
